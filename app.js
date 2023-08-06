fc460c8ddf1ca7edc7f65372fcf90c18b17ab487
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-08-07 00:44:07 +0530

    Updated App.js for songs route using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const songsRoute = require('./routes/songs');
+app.use('/songs', songsRoute);
 
 const conn = require('./middlewares/connection');
 console.log("Trying to establish a connection to TiDB...");
