95f2be6a1992079721caa33c351c3c52bc2ce836
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-08-07 00:45:18 +0530

    Updated app.js using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const grammy  = require('./controllers/grammy');
+app.use('/controller', grammy);
 const songsRoute = require('./routes/songs');
 app.use('/songs', songsRoute);
 
